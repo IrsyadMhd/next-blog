c4e1db0e16e4e76b79a20b939132c79107d1b5e8
IrsyadMhd <mhd.irsyad@gmail.com>
2021-12-23 22:42:51 +0700

    added all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-const AllPostPage = () => {};
+import AllPosts from '../../components/posts/all-posts';
+
+const DUMMY_DATA = [
+  {
+    title: 'Get started with Next-JS',
+    date: '2021-12-12',
+    slug: 'get-started-with-next-js',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js gives you the best developer experience with all the features you need for production.',
+  },
+  {
+    title: 'Get started with Next-JS2',
+    date: '2021-12-12',
+    slug: 'get-started-with-next-js2',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js gives you the best developer experience with all the features you need for production.',
+  },
+  {
+    title: 'Get started with Next-JS3',
+    date: '2021-12-12',
+    slug: 'get-started-with-next-js3',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js gives you the best developer experience with all the features you need for production.',
+  },
+  {
+    title: 'Get started with Next-JS4',
+    date: '2021-12-12',
+    slug: 'get-started-with-next-js4',
+    image: 'getting-started-nextjs.png',
+    excerpt:
+      'Next.js gives you the best developer experience with all the features you need for production.',
+  },
+];
+
+const AllPostPage = () => {
+  return <AllPosts posts={DUMMY_DATA} />;
+};
 
 export default AllPostPage;
