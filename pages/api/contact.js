f4b323bf10ed702eda0f681cd818085ad52706b8
IrsyadMhd <mhd.irsyad@gmail.com>
2021-12-25 15:03:45 +0700

    storing messages to mongoDB

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-const handler = (req, res) => {
+import { MongoClient } from 'mongodb';
+
+const handler = async (req, res) => {
   if (req.method === 'POST') {
     const { email, name, message } = req.body;
 
@@ -19,7 +21,30 @@ const handler = (req, res) => {
       email,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://dbird:2t7FQs-auK4EM65@cluster0.sn1xx.mongodb.net/my-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Failed connected to database' });
+      return;
+    }
+
+    const db = client.db();
+
+    try {
+      const result = await db.collection('message').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      res.status(500).json({ message: 'Failed insert message!' });
+      client.close();
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({ message: 'success', newMessage });
   }
