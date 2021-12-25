fc245e9ad72d53f7475677dab5a5b6fdcc25354c
IrsyadMhd <mhd.irsyad@gmail.com>
2021-12-25 13:38:56 +0700

    use markdown data for allposts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,42 +1,18 @@
 import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../lib/post-util';
 
-const DUMMY_DATA = [
-  {
-    title: 'Get started with Next-JS',
-    date: '2021-12-12',
-    slug: 'get-started-with-next-js',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'Next.js gives you the best developer experience with all the features you need for production.',
-  },
-  {
-    title: 'Get started with Next-JS2',
-    date: '2021-12-12',
-    slug: 'get-started-with-next-js2',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'Next.js gives you the best developer experience with all the features you need for production.',
-  },
-  {
-    title: 'Get started with Next-JS3',
-    date: '2021-12-12',
-    slug: 'get-started-with-next-js3',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'Next.js gives you the best developer experience with all the features you need for production.',
-  },
-  {
-    title: 'Get started with Next-JS4',
-    date: '2021-12-12',
-    slug: 'get-started-with-next-js4',
-    image: 'getting-started-nextjs.png',
-    excerpt:
-      'Next.js gives you the best developer experience with all the features you need for production.',
-  },
-];
-
-const AllPostPage = () => {
-  return <AllPosts posts={DUMMY_DATA} />;
+const AllPostPage = props => {
+  return <AllPosts posts={props.posts} />;
 };
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostPage;
